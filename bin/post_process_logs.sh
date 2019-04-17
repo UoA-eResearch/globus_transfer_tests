fe2bbd391b6ec8e23d05e9fbbb8d0def42cbcf5d
Martin Feller <m.feller@auckland.ac.nz>
2019-04-17 17:59:36 +1200

    remove unit in transfer rate column

--- bin/post_process_logs.sh
+++ bin/post_process_logs.sh
@@ -17,6 +17,7 @@ echo "date|src_and_dest|folder|transfer_rate|transfer_options|label"
 for f in $@; do
   cat ${f} | grep RESULT | cut -d\| -f1,4,6,8,10,11,12 | \
      sed "s/${nesi2uoa}/${nesi2uoa_subst}/g" | \
-     sed "s/${uoa2nesi}/${uoa2nesi_subst}/g"
+     sed "s/${uoa2nesi}/${uoa2nesi_subst}/g" | \
+     sed "s;MB/s;;g"
 done
 
