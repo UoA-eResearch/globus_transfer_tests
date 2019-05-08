3206555ff5b638ee9462c5f92b2706d479b1134d
Martin Feller <m.feller@auckland.ac.nz>
2019-05-08 17:22:20 +1200

    refactor to specify type of transfer

--- bin/globus_transfer.sh
+++ bin/globus_transfer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Run directory transfers specified in a task file
-# Task file syntax: src_endpoint | src_path | dest_endpoint | dest_path | repeats | transfer_options | label
-# Currently only directory transfers are allowed
+# Run transfers specified in a task file
+# Task file syntax: src_endpoint | src_path | dest_endpoint | dest_path | transfer_type (file or folder) | repeats | transfer_options | label
 # Output is written to transfer_<current_date_and_time>.log
 # Prerequisites:
 #   * Globus CLI is installed
+#   * Logged in to globus via the CLI
 #   * Endpoints listed in task file have been activated
 
 mylog() {
@@ -14,6 +14,7 @@ mylog() {
     echo "${now} | ${message}" >> ${log_file}
 }
 
+# verify an endpoint has been activated
 verify_endpoint_activated() {
     ep_id="$1"
     globus endpoint is-activated ${ep_id} > /dev/null 2> /dev/null
@@ -23,32 +24,39 @@ verify_endpoint_activated() {
     fi
 }
 
-delete_folder() {
-    verify_endpoint_activated $1
-    url="$1:$2"
-    globus ls ${url} > /dev/null 2> /dev/null
-    if [ "$?" -eq "0" ]; then
-        cmd="globus delete --notify off ${url} --recursive"
-        mylog "${cmd}"
-        task_id=$(${cmd} | grep "Task ID:" | cut -d: -f2 | xargs)
-        mylog "Waiting for deletion to finish (Task ID: ${task_id})"
-        globus task wait ${task_id}
+# delete a file or a folder via globus
+delete() {
+    endpoint=$1
+    path=$2
+    transfer_type=$3
+    verify_endpoint_activated ${endpoint}
+    if [ "${transfer_type}" == "folder" ]; then
+        cmd="globus rm -r -f --notify off ${endpoint}:${path}"
+    else
+        cmd="globus rm -f --notify off ${endpoint}:${path}"
     fi
+    mylog "${cmd}"
+    ${cmd} > /dev/null 2> /dev/null
 }
 
-transfer_folder() {
+# transfer a file or a folder via globus
+transfer() {
     src_ep=$1
     src_path=$2
     dst_ep=$3
     dst_path=$4
-    transfer_options="${5}"
-    label="${6}"
-    folder=$(echo ${src_path} | rev | cut -d/ -f1 | rev)
+    transfer_options="$5"
+    transfer_type="$6"
+    label="$7"
+    file_or_folder=$(echo ${src_path} | rev | cut -d/ -f1 | rev)
 
     verify_endpoint_activated ${src_ep}
     verify_endpoint_activated ${dst_ep}
 
-    cmd="globus transfer --notify off ${transfer_options} "${src_ep}:${src_path}" "${dst_ep}:${dst_path}" --recursive"
+    cmd="globus transfer --notify off ${transfer_options} ${src_ep}:${src_path} ${dst_ep}:${dst_path}"
+    if [ "${transfer_type}" == "folder" ]; then
+        cmd="${cmd} --recursive" 
+    fi
     mylog "${cmd}"
     task_id=$(${cmd} | grep "Task ID:" | cut -d: -f2 | xargs)
     mylog "Waiting for transfer to finish (Task ID: ${task_id})"
@@ -61,7 +69,7 @@ transfer_folder() {
     bytes_per_second=$(cat ${tmp_file} | grep "Bytes Per Second:" | cut -d: -f2 | xargs)
     rm -f ${tmp_file}
 
-    mylog "RESULT|${task_id}|${src_ep}|${src_path}|${dst_ep}|${dst_path}|${folder}|${bytes_transferred}|${bytes_per_second}|${transfer_options}|${label}|${status}"
+    mylog "RESULT|${task_id}|${src_ep}|${src_path}|${dst_ep}|${dst_path}|${file_or_folder}|${bytes_transferred}|${bytes_per_second}|${transfer_options}|${label}|${status}"
 }
 
 # verify a task file has been provided on the command-line
@@ -92,13 +100,20 @@ while read line; do
     src_path=$(echo $tmp | cut -d\| -f2 | xargs)
     dst_ep=$(echo $tmp | cut -d\| -f3 | xargs)
     dst_path=$(echo $tmp | cut -d\| -f4 | xargs)
-    repeats=$(echo $tmp | cut -d\| -f5 | xargs)
-    transfer_options=$(echo $tmp | cut -d\| -f6 | xargs)
-    label=$(echo $tmp | cut -d\| -f7 | xargs)
+    transfer_type=$(echo $tmp | cut -d\| -f5 | xargs)
+    repeats=$(echo $tmp | cut -d\| -f6 | xargs)
+    transfer_options=$(echo $tmp | cut -d\| -f7 | xargs)
+    label=$(echo $tmp | cut -d\| -f8 | xargs)
+
+    if [ "${transfer_type}" != "folder" ] && [ "${transfer_type}" != "file" ]; then
+        mylog "WARNING: Wrong transfer type: ${transfer_type}. Only file and folder are allowed. Ignoring request"
+	continue
+    fi
+
     # run each transfer for ${repeats} times
     for i in $(seq ${repeats}); do
-        delete_folder "${dst_ep}" "${dst_path}"
-        transfer_folder ${src_ep} ${src_path} ${dst_ep} ${dst_path} "${transfer_options}" "${label}"
+        delete "${dst_ep}" "${dst_path}" "${transfer_type}"
+        transfer ${src_ep} ${src_path} ${dst_ep} ${dst_path} "${transfer_options}" "${transfer_type}" "${label}"
     done
 done < ${input_file}
 
